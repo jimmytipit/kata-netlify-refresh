fb8923932add9a1b449bca89d268fc16f9a16070
Jimmy McArthur <jimmy@openstack.org>
2019-08-08 11:17:30 -0500

    Update announcement for Shanghai

--- site/.vuepress/components/bar.vue
+++ site/.vuepress/components/bar.vue
@@ -3,14 +3,14 @@
     <div class="container">
       <div class="bar-inner">
         <div class="bar-logo">
-          <img :src="require('../theme/images/bar-logo.svg')" alt="Meet the Airship Community">
+          <img :src="require('../theme/images/bar-logo.svg')" alt="Meet the Kata Community">
         </div>
         <!-- /.bar-logo -->
         <div class="bar-entry">
-          <p>Meet the Kata Containers Community at the Open Infrastructure Summit, April 29–May 1, 2019 in Denver, Colorado!</p>
+          <p>Meet the Kata Containers Community at the Open Infrastructure Summit, November 4-6, 2019 in Shanghai, China!</p>
         </div>
         <!-- /.bar-entry -->
-        <a href="//openstack.org/summit/denver-2019/" target="_blank" class="bar-btn">Learn More</a>
+        <a href="//openstack.org/summit/shanghai-2019/" target="_blank" class="bar-btn">Learn More</a>
       </div>
       <!-- /.bar-inner -->
     </div>
